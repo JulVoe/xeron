d1cc24a8fbccebd5902e1e21ca385c2024b73198
JulVoe <53940541+JulVoe@users.noreply.github.com>
2019-11-02 22:56:50 +0100

    Update intrinsics.cpp

--- Core/Intrinsics/intrinsics.cpp
+++ Core/Intrinsics/intrinsics.cpp
@@ -14,7 +14,7 @@ namespace impl {
 		out1 = _mm_srli_epi32(in, 16);
 		out2 = _mm_and_epi32(out2_tmp, _mm_set1_epi32(0xFF));
 	}
-	//Converts two [u]int32_t[4] (in_lo, in_hi) to [u]int16_t[8] using truncation. If unsafe, we might assume that ever element of in_lo will fit in an int16_t
+	//Converts two [u]int32_t[4] (in_lo, in_hi) to [u]int16_t[8] using truncation. If unsafe, we might assume that every element of in_lo will fit in an int16_t
 	template<bool safe>
 	inline __m128i _mm_compress_epi32_01(const __m128i in_lo, __m128i in_hi) {
 //	    return _mm_shuffle_epi8(_mm_compress_epi32_02(in_lo,in_hi),0xWHATEVER);
