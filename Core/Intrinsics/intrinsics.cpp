02afac09dbb5a354cd3fcb41802e327449bc03b4
JulVoe <53940541+JulVoe@users.noreply.github.com>
2019-11-04 17:42:06 +0100

    Update intrinsics.cpp

--- Core/Intrinsics/intrinsics.cpp
+++ Core/Intrinsics/intrinsics.cpp
@@ -47,12 +47,25 @@ namespace impl {
 		return _mm_or_si128(hi_epi32_shift, lo_epi32_mask);
 #endif
 	}
+	//Converts two int32_t[4] (in_lo, in_hi) to int16_t[8]
+	//If SSE4.1 is available, truncation is used, otherwiese saturation. 
 	inline __m128i _mm_compress_epi32_21(const __m128i in_lo, const __m128i in_hi) {
 #if SSE >= 41
 		const __m128i hi_epi32_shift = _mm_slli_epi32(in_hi, 16);
 		return _mm_blend_epi16(in_lo, hi_epi32_shift, 0xAA);
 #else
 	    return _mm_shuffle_epi8(_mm_compress_epi32_10(in_lo,in_hi),_mm_setr_epi8(0,1,8,9,2,3,10,11,4,5,12,13,6,7,14,15));
+#endif
+	}
+	//Converts two uint32_t[4] (in_lo, in_hi) to uint16_t[8]
+	//If SSE4.1 is available, truncation is used, otherwiese saturation.
+	//Note: If SSE4.1 is not available, no highest bit of the input should be set or the output will be 0 (see _mm_compress_epu32_10)
+	inline __m128i _mm_compress_epi32_21(const __m128i in_lo, const __m128i in_hi) {
+#if SSE >= 41
+		const __m128i hi_epi32_shift = _mm_slli_epi32(in_hi, 16);
+		return _mm_blend_epi16(in_lo, hi_epi32_shift, 0xAA);
+#else
+	    return _mm_shuffle_epi8(_mm_compress_epu32_10(in_lo,in_hi),_mm_setr_epi8(0,1,8,9,2,3,10,11,4,5,12,13,6,7,14,15));
 #endif
 	}
 
